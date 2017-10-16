cc7f187a9a666778f2c0cb93c536d197bb2c6e3b
felipe.reblin <felipe.reblin@LAB0727.catolicasc.org.br>
2017-10-16 21:41:43 -0200

    commit inicial (1042)

---
+++ src/main/Main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import Uri1042.Uri1042;
+
+/**
+ *
+ * @author felipe.reblin
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        switch (args[0]) {
+            case "1042":
+                Uri1042.run();
+                break;
+        }
+        
+        
+        
+    }
+    
+}
